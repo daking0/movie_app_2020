6e22b7281e93be0b71e66c0402dfac35ab61d58c
daking0 <angk226@gmail.com>
2020-11-28 23:37:02 +0900

    state 끝

--- src/App.js
+++ src/App.js
@@ -2,43 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component{
-
-  // constructor(){
-
-  // }
-
   state = {
-    count : 0
-  };
-  
-  add = () => {
-    this.setState(current => ({ count: current.count +1 }))
-  };
-  minus = () => {
-    this.setState({ count: this.state.count -1 })
+    isLoading : true
   };
 
   componentDidMount(){
-    console.log("component rendered");
-  };
-
-  componentDidUpdate(){
-    console.log("I just updated");
-  };
-
-  componentWillUnmount(){
-    console.log("Goodbte");
+    setTimeout(() =>{
+      this.setState({isLoading : false })
+    }, 6000);
   }
-
   render(){  
-    console.log("component rendering");
+    const {isLoading} = this.state;
     return (
     <div>
-      <h1>The number is {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}> Minus</button>
-      </div>
-      )
+      {this.state.isLoading ? "Loading" : "we are ready"}
+    </div>
+    )
   }
 }
 
